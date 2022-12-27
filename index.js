3cfc07307da5940ad7080108786fb34d2e6058b2
shipatulhasan <shipatulhasan328@gmail.com>
2022-12-28 00:40:07 +0600

    added post route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -31,6 +31,12 @@ const run = async()=>{
             const task = req.body
             const result = await tasksCollection.insertOne(task)
             res.send(result)
+        })
+        app.delete('/task/:id',async(req,res)=>{
+            const id = req.params.id
+            const find = {_id:ObjectId(id)} 
+            const result = await tasksCollection.deleteOne(find)
+            res.send({result,id})
         })
 
     }
